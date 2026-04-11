0f71a29650392ad094ef9f1f631b1fff4d16a3e5
Durgesh Bhatt <durgeshbhatt360@gmail.com>
2026-04-11 15:57:34 +0530

    created HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button variant='outline'>Shadcn button</Button>
+export default function Home(){
+  return <div className="relative w-full">
+    <BgGradient/>
+    <div className="flex flex-col">
+      <HeroSection/>
     </div>
-  );
+    {/* <DemoSection/> */}
+    {/* <HowItWorksSection/> */}
+    {/* <PricingSection/> */}
+    {/* <CTASection/> */}
+
+  </div>
 }
