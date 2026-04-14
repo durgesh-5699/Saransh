fed8101b1570931da89a8af71fcf5895095f9cd2
Durgesh Bhatt <durgeshbhatt360@gmail.com>
2026-04-14 16:40:24 +0530

    created cta-section and pricing-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
 import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
 import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -11,9 +13,9 @@ export default function Home() {
         <HeroSection />
         <DemoSection />
       <HowItWorksSection />
+      <PricingSection/>
+      <CTASection/>
       </div>
-      {/* <PricingSection/> */}
-      {/* <CTASection/> */}
     </div>
   );
 }
